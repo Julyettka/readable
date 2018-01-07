eb87e9151851618445eb8295956ecc41a7cc8ab9
julia <julyettka@gmail.com>
2018-01-07 19:23:23 +0200

    fix: del button when adding post route

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { Component } from 'react'
 
+class Header extends Component{
+	currentRoute(){
+		let url = window.location.href;
+		url = url.split('/');
+		let ln = url.length;
+		url = url[ln - 1];
+		return url;
+	}
 
-const Header = ()=>(
-    <div className="header">
-        <div className="container-header">
-            <Link to='/' style={{textDecoration:'none'}}><div className="logo">Blog
-            	<span className="descr">about everything you wish to know</span>
-            </div></Link>
-            	<Link to='/add'><button className="post-button">New Post</button></Link>
-        </div>
-    </div>
-    )
+	render(){
+		const curRoute = this.currentRoute();
+		return(
+	    <div className="header">
+	        <div className="container-header">
+	            <Link to='/' style={{textDecoration:'none'}}>
+	            	<div className="logo">Blog
+	            		<span className="descr">about everything you wish to know</span>
+	            	</div>
+	            </Link>
+	            {curRoute == 'add' ? '' : <Link to='/add'><button className="post-button">New Post</button></Link>}
+	        </div>
+	    </div>
+	    )
+	}
+}
 
 export default Header;
